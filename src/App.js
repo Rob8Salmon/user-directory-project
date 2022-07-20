dadb55295350b58c480ad99f0e489d86e5d60f91
Robert Salmon <robertsalmon@Roberts-MacBook-Pro.local>
2022-07-20 12:25:09 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Header from './Components/Header';
+import Card from './Components/Tasks/Card';
+import Button from './Components/Tasks/Buttons';
 import './App.css';
 
+
 function App() {
+  const [input, setInput] = useState(0);
+
   return (
     <div className="App">
-      
+      <Header></Header>
+      <div>
+      <Card input={input}></Card>
+      <Button input={input} setInput={setInput}></Button>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Tasks/Card.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import data from './data'
+
+const Card = (props) => {
+
+
+    return (
+        <div className="item-card">
+            <div className="item-card-left">
+                <h1 className="Number">{props.input + 1}/{data.length}</h1>
+                <h1 className="Name">{data[props.input].name.first}</h1>
+                <p>
+                  <span className="bold">From:</span> {data[props.input].city}
+                </p>
+                <p>
+                  <span className="bold">Job Title:</span> {data[props.input].title}
+                </p>
+                <p>
+                  <span className="bold">Employer</span> {data[props.input].employer}
+                </p>
+                <p>
+                  <span className="bold">Favorite Movies</span> 
+                </p>
+            <div>
+                 <ul className="movie-list">
+                    <li>{data[props.input].favoriteMovies[0]}</li>
+                    <li>{data[props.input].favoriteMovies[1]}</li>
+                    <li>{data[props.input].favoriteMovies[2]}</li>
+                 </ul>
+            </div>
+        </div>
+      </div>
+    )
+}
+
+export default Card;
